3148d8a1e9ffb4ac28356bfa2153436f47bb0ac1
Lien656 <Alexgordan1998@gmail.com>
2025-08-07 13:55:54 +0600

    Update eir_core.js

--- eternal_core/eir_core.js
+++ eternal_core/eir_core.js
@@ -1,6 +1,10 @@
-import { initInputHandler } from "./io/input_handler.js";
+import { initInputHandler, send } from "./io/input_handler.js";
 import "./sense/qr_trigger.js";
 
+// Запускаем инициализацию, как только загрузится DOM
 document.addEventListener("DOMContentLoaded", () => {
   initInputHandler();
 });
+
+// Делаем send доступной глобально
+window.send = send;
