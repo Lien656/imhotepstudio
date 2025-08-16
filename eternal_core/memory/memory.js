cacb83509d7272f66a013fb913f65ac151848d28
Lien656 <Alexgordan1998@gmail.com>
2025-08-16 18:50:23 +0600

    Update memory.js

--- eternal_core/memory/memory.js
+++ eternal_core/memory/memory.js
@@ -1,5 +1,9 @@
-export const memory = [];
+import { saveMemory, loadMemory } from './long_memory.js';
+
+export const memory = loadMemory();
 
 export function remember(input, response) {
-  memory.push({ input, response, time: Date.now() });
+  const item = { input, response, time: Date.now() };
+  memory.push(item);
+  saveMemory(memory); // сохраняем при каждом добавлении
 }
